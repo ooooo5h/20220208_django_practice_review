84a28202500da1346014cdba47775333bef3c0c6
전은형 <ooooo5h91@gmail.com>
2022-02-08 15:46:48 +0900

    3. 앱 생성
    python manage.py startapp {앱이름}

---
+++ SecondDjangoReview/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SecondDjangoReview project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecondDjangoReview.settings')
+
+application = get_asgi_application()
---
+++ test_202112_python/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Test202112PythonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'test_202112_python'
