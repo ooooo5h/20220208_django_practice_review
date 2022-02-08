f3f3b207e465345ac1789d974f9b6e81fd8bfbab
전은형 <ooooo5h91@gmail.com>
2022-02-08 16:08:54 +0900

    11. api폴더를 생성해 lectures의 기능을 생성중인데 그대로 내려주면 안됨!
    -  DB조회결과는  dict형태가 아니므로 JSON으로 가공이 불가능함
    -  모델 클래스 내부에 메쏘드를 추가하여, 실행결과를 dict로 변경해서 JSON응답에 활용시켜주자

---
+++ test_202112_python/api/lectures/lecture.py
@@ -0,0 +1,19 @@
+from django.views import View
+from django.http import HttpResponse
+from test_202112_python.models import Lectures
+
+class Lecture(View):
+    
+    # 특정 강의 내려주기 => 이름으로 조회?
+    def get(self, request): 
+                
+        search_lecture = Lectures.objects.filter(name=request.GET['name']).first()
+        
+        
+        return HttpResponse({
+            'code' : 200,
+            'message' : '특정 강의 내려주기',
+            'data' : {
+                'lecture' : search_lecture.
+            }
+        })
